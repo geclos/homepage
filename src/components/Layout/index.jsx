6244cfca4245146b061679b78fbfb40ad32b0fc8
Gerard Clos <clos.gerard@gmail.com>
2020-05-06 20:19:39 +0200

    Revert "load custom fonts asynchronously"
    This reverts commit d3ca79bc666d9eba4986fd44a33379c6d8f8dad8.

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types'
 import React from "react"
+import { Helmet } from 'react-helmet'
 
 import './index.scss'
 
 const Layout = ({ children }) => {
   return (
     <main>
+      <Helmet>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;400;500;700&display=swap" rel="stylesheet" />
+      </Helmet>
       {children}
     </main>
   )
